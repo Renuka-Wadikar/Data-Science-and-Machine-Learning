6a5c36dac99bbeb9afbb7cc807e0e3381a0ff2ad
renuka wadi <revadarshan27@gmail.com>
2023-07-31 20:02:51 +0530

    adding exception and logging files

---
+++ End_to_End_Project/src/exception.py
@@ -0,0 +1,32 @@
+import sys
+from typing import Any
+import logging
+
+
+def error_msg_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_msg = "Error Occured in python script [{0}] at line numer [{1}] error message[{2}]".format(
+        file_name,
+        exc_tb.tb_lineno,
+        str(error)
+    )
+    
+    return error_msg
+    
+class CustomException(Exception):
+    def __init__(self,error_msg, error_detail:sys ):
+        super().__init__(error_msg)
+        self.error_msg = error_msg_detail(error = error_msg,error_detail = error_detail)
+        
+    def __str__(self):
+        return self.error_msg
+    
+
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Logging has started>>>')
+        raise CustomException(e,sys)
+        
